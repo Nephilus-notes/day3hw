7d10f6e436ac9b3dd9bc41ed779ec7186c6e2ad6
Nephilus-notes <114515979+Nephilus-notes@users.noreply.github.com>
2022-11-10 10:37:23 -0600

    fixed module placement

---
+++ mathy_module.py
@@ -0,0 +1,9 @@
+import math
+
+def sqr_house(length, width):
+    return length*width
+
+
+def pie(radius):
+    return math.pi*radius*2
+    
---
+++ text.py
@@ -0,0 +1,3 @@
+text = {
+    'task_query' : """Would you like \n(Square footage) to calculate the square footage of a house?
+(Circumference) To calculate the circumference of a circle? \nOr (Quit)? """}
